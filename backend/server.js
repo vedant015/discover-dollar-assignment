96d255483b0bc00cc9ff2d825dc030b132963970
Vedant015 <vedantgirawale013@gmail.com>
2026-02-24 22:17:02 +0530

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
-// parse requests of content-type - application/json
-app.use(express.json());
+app.use(cors());
 
-// parse requests of content-type - application/x-www-form-urlencoded
+// parse requests
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
+
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
@@ -30,8 +31,8 @@ app.get("/", (req, res) => {
 
 require("./app/routes/turorial.routes")(app);
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}.`);
 });
